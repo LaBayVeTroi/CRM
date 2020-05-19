7d46a8b7a38f8cd5fd677b76ae6494555094b0c9
labayvetroi <namnguyenuetk62@gmail.om>
2020-05-19 07:45:58 +0700

    fix persistence config

--- event/src/main/java/com/teko/event/EventApplication.java
+++ event/src/main/java/com/teko/event/EventApplication.java
@@ -3,15 +3,16 @@ package com.teko.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages= {"com.teko"})
+@SpringBootApplication(scanBasePackages = {"com.teko","config"})
 @EnableJpaRepositories(
         basePackages = "com.teko.repository",
         entityManagerFactoryRef = "entityManager"
 )
-public class EventApplication{
-
+@EntityScan(basePackages = "com.teko.domain")
+public class EventApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
     }
