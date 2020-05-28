55ae0bb4f19b788ceea137513f2f6d16d934010b
labayvetroi <namnguyenuetk62@gmail.com>
2020-05-28 14:22:58 +0700

    change run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-mvn -Dmaven.test.skip=true clean install
-java -jar ./domain/target/domain-0.0.1-SNAPSHOT-exec.jar
-java -jar ./account/target/account-0.0.1-SNAPSHOT.jar &
-java -jar ./common/target/common-0.0.1-SNAPSHOT.jar &
-java -jar ./contact/target/contact-0.0.1-SNAPSHOT.jar &
-java -jar ./email/target/email-0.0.1-SNAPSHOT.jar &
-java -jar ./event/target/event-0.0.1-SNAPSHOT.jar &
-java -jar ./invoice/target/invoice-0.0.1-SNAPSHOT.jar &
-java -jar ./lead/target/lead-0.0.1-SNAPSHOT.jar &
-java -jar ./opportunity/target/opportunity-0.0.1-SNAPSHOT.jar &
-java -jar ./planner/target/planner-0.0.1-SNAPSHOT.jar &
-java -jar ./task/target/task-0.0.1-SNAPSHOT.jar &
-java -jar ./team/target/team-0.0.1-SNAPSHOT.jar &
+#mvn -Dmaven.test.skip=true clean install
+java -jar ./build/domain-0.0.1-SNAPSHOT-exec.jar
+java -jar ./build/account-0.0.1-SNAPSHOT.jar &
+java -jar ./build/common-0.0.1-SNAPSHOT.jar &
+java -jar ./build/contact-0.0.1-SNAPSHOT.jar &
+java -jar ./build/email-0.0.1-SNAPSHOT.jar &
+java -jar ./build/event-0.0.1-SNAPSHOT.jar &
+java -jar ./build/invoice-0.0.1-SNAPSHOT.jar &
+java -jar ./build/lead-0.0.1-SNAPSHOT.jar &
+java -jar ./build/opportunity-0.0.1-SNAPSHOT.jar &
+java -jar ./build/planner-0.0.1-SNAPSHOT.jar &
+java -jar ./build/task-0.0.1-SNAPSHOT.jar &
+java -jar ./build/team-0.0.1-SNAPSHOT.jar &
