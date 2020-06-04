14af6ade6c356665e6286523ab91023e283b3948
labayvetroi <namnguyenuetk62@gmail.com>
2020-06-04 10:43:52 +0700

    fix ram cosuming

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-java -jar ./domain-0.0.1-SNAPSHOT-exec.jar &
-java -jar ./account-management-0.0.1-SNAPSHOT.jar &
-java -jar ./task-management-0.0.1-SNAPSHOT.jar &
+java -jar -Xmx512m  ./domain-0.0.1-SNAPSHOT-exec.jar &
+java -jar -Xmx512m ./account-management-0.0.1-SNAPSHOT.jar &
+java -jar -Xmx512m ./task-management-0.0.1-SNAPSHOT.jar &
 wait
 tail run.sh
